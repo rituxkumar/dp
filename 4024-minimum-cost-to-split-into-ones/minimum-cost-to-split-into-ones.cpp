f54c7a923dedd59fa6c5d44e7b83bbfbad219b07
Ritu Kumar <the.ritukumar@gmail.com>
2026-05-04 18:47:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (31.27%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-int res = 0;
-    void solve(int n){
-        if(n<=1){
-            return ;
+    int dp[501];
+    int solve(int n) {
+        if (n <= 1) {
+            return 0;
         }
-        res+=n-1;
-        solve(n-1);
-
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+     
+        return dp[n] =n-1+ solve(n - 1);
     }
     int minCost(int n) {
         // return n*(n-1)/2;
+        memset(dp, -1, sizeof(dp));
+       return solve(n);
 
-       solve(n);
-       return res;
     }
 };

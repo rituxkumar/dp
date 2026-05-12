50321cbbe6a518e6a36e7fff2dec8f95c5b69958
Ritu Kumar <the.ritukumar@gmail.com>
2026-05-12 13:24:43 +0530

    Time: 6 ms (8.79%) | Memory: 8.7 MB (97.39%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int n = word.length();
+        int count = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (word[i] == word[i - 1]) {
+                count++;
+            }
+        }
+        return count + 1;
+    }
+};

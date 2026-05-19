1d84f37fed9f2addae42c8344a2ca73583b86b02
Ritu Kumar <the.ritukumar@gmail.com>
2026-05-19 13:49:49 +0530

    Time: 169 ms (45.97%) | Memory: 9.8 MB (87.92%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[301][301];
+    int M = 1e9+7;
+    int solve(int n, int num, int x) {
+        if (n == 0) {
+            return 1;
+        }
+        int currPow = pow(num, x);
+        if (currPow > n) {
+            return 0;
+        }
+        if (dp[n][num] != -1) {
+            return dp[n][num];
+        }
+
+        int take = solve(n - currPow, num + 1, x);
+
+        int skip = solve(n, num + 1, x);
+
+        return dp[n][num] = (take + skip)%M;
+    }
+    int numberOfWays(int n, int x) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n, 1, x);
+    }
+};

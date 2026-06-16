2d8f76fb5d0dbabaf73f399f2d1b80d78eafd288
Ritu Kumar <the.ritukumar@gmail.com>
2026-06-16 23:26:48 +0530

    Time: 5 ms (45.83%) | Memory: 39.6 MB (87.50%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result = "";
+        for (char& ch : s) {
+            if (ch == '*') {
+                if (result.length() > 0)
+                    result.pop_back();
+            } else if (ch == '#') {
+                result += result;
+            } else if (ch == '%') {
+                reverse(result.begin(), result.end());
+            } else {
+                result += ch;
+            }
+        }
+        return result;
+    }
+};
